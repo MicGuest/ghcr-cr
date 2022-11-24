c5ccf34afe8f59333661fecf318d1f560ccc6d1d
MicGuest <118722862+MicGuest@users.noreply.github.com>
2022-11-24 15:04:11 +0530

    Update main.py

--- webapp/main.py
+++ webapp/main.py
@@ -1,23 +1,33 @@
 from transformers import pipeline
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+#from pydantic import BaseModel
 
-generator = pipeline('text-generation', model='gpt2')
+# main.py
+
+#from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-class Body(BaseModel):
-    text: str
+# generator = pipeline('text-generation', model='gpt2')
+
+# app = FastAPI()
 
 
-@app.get('/')
-def root():
-    return HTMLResponse("<h1>A self-documenting API to interact with a GPT2 model and generate text</h1>")
+# class Body(BaseModel):
+#     text: str
 
 
-@app.post('/generate')
-def predict(body: Body):
-    results = generator(body.text, max_length=35, num_return_sequences=1)
-    return results[0]
+# @app.get('/')
+# def root():
+#     return HTMLResponse("<h1>A self-documenting API to interact with a GPT2 model and generate text</h1>")
+
+
+# @app.post('/generate')
+# def predict(body: Body):
+#     results = generator(body.text, max_length=35, num_return_sequences=1)
+#     return results[0]
